46abcb37a6ccc43755d8fba34c0e318477d4a2a9
Lenara Ventura <lenara_ventura@estudante.sesisenai.org.br>
2023-06-14 21:46:12 -0300

    Criação JavaClass do package Modelos
    Criado os package (modelos, services,controles,repositorios). Criado JavaClass: Usuario, Estoque, Armazem

---
+++ src/main/java/br/com/demo/devinAdotion/modelos/Armazem.java
@@ -0,0 +1,35 @@
+package br.com.demo.devinAdotion.modelos;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Table(name = "tb_armazem")
+public class Armazem {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Long id;
+
+    //@NotBlank(message = " O preenchimento deste campo é obrigatório. ")
+    @Column(nullable = false, unique = true)
+    private  String nome;
+
+    @Column(nullable = false)
+    private String animal;
+
+    @Column(nullable = false) // colocar a regra no front de vir defaul "true"
+    private Boolean situacao;
+
+
+}
