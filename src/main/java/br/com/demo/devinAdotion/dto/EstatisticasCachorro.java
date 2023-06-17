97ccd6755a3934e691c00b4f66e8c21ffe8fe407
julianoppinheiro <juliano_p_pinheiro@estudante.sesisenai.org.br>
2023-06-16 22:15:31 -0300

    Dashboard
    Feita e refatoração do código,  criado o dto, service, controle e implementação do count no EstoqueRepositorio

--- src/main/java/br/com/demo/devinAdotion/dto/EstatisticasCachorro.java
+++
@@ -1,9 +0,0 @@
-package br.com.demo.devinAdotion.dto;
-
-public class EstatisticasCachorro
-{
-    private long totalCachorro;
-    private long totalCachorroComAntipulgas;
-    private long totalCachorroComAntiparasitario;
-    private double mediaRacaoCachorro;
-}
