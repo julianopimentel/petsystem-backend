25d008929077ea04d11d448d8cdbbca88ac14aad
Lenara Ventura <lenara_ventura@estudante.sesisenai.org.br>
2023-06-20 19:35:47 -0300

    Ajustes nos métodos do editar usuario e armazem
    Ajustes nos métodos do editar usuario e armazem

--- src/main/java/br/com/demo/devinAdotion/controles/UsuarioControle.java
+++ src/main/java/br/com/demo/devinAdotion/controles/UsuarioControle.java
@@ -1,46 +1,51 @@
-//package br.com.demo.devinAdotion.controle;
-//
-//import br.com.demo.devinAdotion.modelos.Usuario;
-//import br.com.demo.devinAdotion.servicos.UsuarioServico;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/usuarios")
-//public class UsuarioControle {
-//
-//    private final UsuarioServico usuarioServico;
-//
-//    @Autowired
-//    public UsuarioControle(UsuarioServico usuarioServico) {
-//        this.usuarioServico = usuarioServico;
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
-//        Usuario novoUsuario = usuarioServico.cadastrarUsuario(usuario);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<List<Usuario>> listarUsuarios() {
-//        List<Usuario> usuarios = usuarioServico.listarUsuarios();
-//        return ResponseEntity.ok(usuarios);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
-//        Usuario usuario = usuarioServico.buscarUsuarioPorId(id);
-//        if (usuario != null) {
-//            return ResponseEntity.ok(usuario);
-//        }
-//        return ResponseEntity.notFound().build();
-//    }
-//
+package br.com.demo.devinAdotion.controles;
+
+import br.com.demo.devinAdotion.modelos.Usuario;
+import br.com.demo.devinAdotion.servicos.UsuarioServico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioControle {
+
+    private final UsuarioServico usuarioServico;
+
+    @Autowired
+    public UsuarioControle(UsuarioServico usuarioServico) {
+        this.usuarioServico = usuarioServico;
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+        Usuario novoUsuario = usuarioServico.cadastrarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        List<Usuario> usuarios = usuarioServico.listarUsuarios();
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
+        Usuario usuario = usuarioServico.buscarUsuarioPorId(id);
+        if (usuario != null) {
+            return ResponseEntity.ok(usuario);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Usuario> atualizarUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioServico.atualizarUsuario(usuario));
+    }
+
 //    @PutMapping("/{id}")
 //    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioAtualizado) {
 //        Usuario usuario = usuarioServico.buscarUsuarioPorId(id);
@@ -51,14 +56,14 @@
 //        }
 //        return ResponseEntity.notFound().build();
 //    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> excluirUsuario(@PathVariable Long id) {
-//        Usuario usuario = usuarioServico.buscarUsuarioPorId(id);
-//        if (usuario != null) {
-//            usuarioServico.excluirUsuario(usuario);
-//            return ResponseEntity.noContent().build();
-//        }
-//        return ResponseEntity.notFound().build();
-//    }
-//}
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirUsuario(@PathVariable Long id) {
+        Usuario usuario = usuarioServico.buscarUsuarioPorId(id);
+        if (usuario != null) {
+            usuarioServico.excluirUsuario(usuario);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
